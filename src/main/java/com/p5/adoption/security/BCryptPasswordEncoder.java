2e7f53d92ec3c76a7692d7af45153fd66d2731cc
Turing99 <andrei.fratila99@yahoo.ro>
2021-06-23 23:58:44 +0300

    Security

---
+++ src/main/java/com/p5/adoption/security/BCryptPasswordEncoder.java
@@ -0,0 +1,9 @@
+package com.p5.adoption.security;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BCryptPasswordEncoder extends org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+{
+
+}
---
+++ src/main/java/com/p5/adoption/security/UserRepository.java
@@ -0,0 +1,14 @@
+package com.p5.adoption.security;
+
+import com.p5.adoption.users.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByEmail(String email);
+
+}
